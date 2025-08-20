b2d0dbf276076ca732c6637e97965c95454fda72
Matias <matiasdamiangodoy@hotmail.com>
2025-08-20 19:24:14 -0300

    agregado post  nuevo producto + filesiystem actualizado

--- TP2/fakestoreapi.js
+++ TP2/fakestoreapi.js
@@ -4,6 +4,21 @@ const fs = require("fs");
 // URL
 const API_URL = "https://fakestoreapi.com/products";
 
+//nuevo producto
+producto_nuevo ={
+    "id": 0,
+    "title": "Mouse Gamer Bag",
+    "price": 22.25,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+}
+
+
 // GET Productos
 async function getProducts(limit) {
   try {
@@ -26,6 +41,35 @@ async function getProducts(limit) {
   }
 }
 
+// POST Productos - Agrega nuevo producto al archivo .json
+async function postProduct(producto_nuevo){
+  try{
+    // Leemos el archivo 'productos.json' de forma asíncrona, usando promesas.
+    const datos = await fs.promises.readFile('./productos.json', 'utf-8');//esto va a retornar una promesa
+    //obtenemos datos y los paamos a un array
+    const productos = JSON.parse(datos);   
+    //busca de los productos el mayor
+    if (productos.length > 0) {
+        maxId = Math.max(...productos.map(producto => producto.id));
+    }
+    //asigna un nuevo id al producto nuevo con el id ultimo +1
+    producto_nuevo.id=maxId+1;
+    //Agregamos el nuevo producto al final del array
+    productos.push(producto_nuevo);
+    // convertimos el array de productos actualizaon a una cadeno json
+    const productosActualizados = JSON.stringify(productos,null,2);
+    //escribo en el archivo con el nuevo producto
+    await fs.promises.writeFile('./productos.json',productosActualizados);
+    //informo
+    console.log("Nuevo Producto ingresado exitosamente");
+
+  }catch(error){
+    console.error("Error al agregar producto:", error);
+  }
+
+}
+
+
 
 // Guardar productos en archivo JSON
 function saveToFile(filename, data) {
@@ -43,6 +87,10 @@ function saveToFile(filename, data) {
     await getProducts(); 
     limitedProducts = await getProducts(5); // Limitado a 5 productos
     saveToFile("productos.json", limitedProducts);
+    //Agrego nuevo producto nuevo
+    await postProduct(producto_nuevo);
+
+
   } catch (err) {
     console.error("Ocurrió un error en la ejecución: ", err);
   }
